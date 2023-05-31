64dc547b359d83c2099723e66a10736b0eef3150
Leong Zhe Cheng <zhecheng555@gmail.com>
2023-05-31 14:56:21 +0800

    Initial Commit

---
+++ MeetingOptimiser/MeetingOptimiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingOptimiserApp.swift
+//  MeetingOptimiser
+//
+//  Created by Leong Zhe Cheng on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetingOptimiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
