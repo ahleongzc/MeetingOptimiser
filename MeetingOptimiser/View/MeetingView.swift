7603223249fb893aeaec8e9c65edfc5af21809f5
Leong Zhe Cheng <zhecheng555@gmail.com>
2023-06-04 16:19:29 +0800

    tried out using chatgpt

--- MeetingOptimiser/View/MeetingView.swift
+++ MeetingOptimiser/View/MeetingView.swift
@@ -6,49 +6,94 @@
 //
 
 import SwiftUI
+import Combine
 
 struct MeetingView: View {
-    
-    @StateObject var chatgpt = OpenAIViewModel()
-    @State var text = ""
-    @State var models = [String]()
+    @State var chatMessages: [ChatMessage] = []
+    @State var messageText: String = ""
+    @State var cancellables = Set<AnyCancellable>()
+    let ChatGPT: OpenAIViewModel = OpenAIViewModel()
     
     var body: some View {
         VStack {
+                    
             ScrollView {
-                ForEach(models, id: \.self) { string in
-                    Text(string)
+                LazyVStack {
+                    ForEach(chatMessages) { message in
+                        messageView(message: message)
+                    }
                 }
             }
-            
-            Spacer()
-            
+
             HStack {
-                TextField("Type here...", text: $text)
-                Button("Send") {
-                    send()
+                TextField("Enter a message", text: $messageText)
+                    .padding()
+                    .background(.gray.opacity(0.1))
+                    .cornerRadius(12)
+                
+                Button {
+                    sendMessage()
+                } label: {
+                    Text("Send")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.black)
+                        .cornerRadius(22)
                 }
             }
-        }
-        .onAppear {
-            chatgpt.setup()
         }
         .padding()
+        
     }
     
-    func send() {
-        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else { return }
-        
-        models.append("Me: \(text)")
-        chatgpt.send(text: text) { response in
-            DispatchQueue.main.async {
-                self.models.append("ChatGPT: " + response)
-                self.text = ""
-            }
+    func messageView(message: ChatMessage) -> some View {
+        HStack {
+            if message.sender == .me { Spacer() }
+            Text(message.content)
+                .foregroundColor(message.sender == .me ? .white : .black)
+                .padding()
+                .background(message.sender == .me ? .blue : .gray.opacity(0.1))
+                .cornerRadius(16)
+            if message.sender == .chatgpt { Spacer() }
         }
-        
-        
     }
+    
+    func sendMessage() {
+        let myMessage = ChatMessage(id: UUID().uuidString, content: messageText, dateCreated: Date(), sender: .me)
+        chatMessages.append(myMessage)
+        
+        ChatGPT.sendMessage(message: messageText).sink { completion in
+            
+        } receiveValue: { response in
+            guard let textResponse = response.choices.first?.text.trimmingCharacters(in: .whitespacesAndNewlines.union(.init(charactersIn: "\""))) else { return }
+            let gptMessage = ChatMessage(id: response.id, content: textResponse, dateCreated: Date(), sender: .chatgpt)
+            chatMessages.append(gptMessage)
+        }
+        .store(in: &cancellables)
+        
+        messageText = ""
+    }
+}
+
+struct ChatMessage: Identifiable {
+    let id: String
+    let content: String
+    let dateCreated: Date
+    let sender: MessageSender
+}
+
+enum MessageSender {
+    case me
+    case chatgpt
+}
+
+extension ChatMessage {
+    static let sampleMessages = [
+        ChatMessage(id: UUID().uuidString, content: "Sample message from me", dateCreated: Date(), sender: .me),
+        ChatMessage(id: UUID().uuidString, content: "Sample message from chatgpt", dateCreated: Date(), sender: .chatgpt),
+        ChatMessage(id: UUID().uuidString, content: "Sample message from me", dateCreated: Date(), sender: .me),
+        ChatMessage(id: UUID().uuidString, content: "Sample message from chatgpt", dateCreated: Date(), sender: .chatgpt),
+        ]
 }
 
 struct MeetingView_Previews: PreviewProvider {
--- MeetingOptimiser/ViewModel/OpenAIViewModel.swift
+++ MeetingOptimiser/ViewModel/OpenAIViewModel.swift
@@ -6,32 +6,52 @@
 //
 
 import Foundation
-import OpenAISwift
+import Alamofire
+import Combine
 
-final class OpenAIViewModel: ObservableObject {
+class OpenAIViewModel {
     
-    private let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
-    private final let MAXTOKENS: Int = 4096
+    let baseURL = "https://api.openai.com/v1/"
+
+    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
     
-    init() {}
+    init() { }
     
-    private var client: OpenAISwift?
-    
-    func setup() {
-        client = OpenAISwift(authToken: apiKey ?? "")
-    }
-    
-    func send(text: String, completionHandler: @escaping (String) -> ()) {
-        client?.sendCompletion(with: text, maxTokens: MAXTOKENS) { result in
-            switch result {
-            case .success(let model):
-                let output = model.choices?.first?.text ?? "Slow"
-                completionHandler(output)
-            case .failure(let error):
-                print("\(error.localizedDescription)")
-                break
+    func sendMessage(message: String) -> AnyPublisher<OpenAICompletionsResponse, Error> {
+        let body = OpenAICompletionsBody(model: "text-davinci-003", prompt: message, temperature: 0.7, max_tokens: 256)
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(apiKey ?? "")"
+        ]
+        
+        return Future { [weak self] promise in
+            guard let self = self else { return }
+            
+            AF.request(self.baseURL + "completions", method: .post, parameters: body, encoder: .json, headers: headers).responseDecodable(of: OpenAICompletionsResponse.self) { response in
+                switch response.result {
+                case .success(let result):
+                    promise(.success(result))
+                case .failure(let error):
+                    promise(.failure(error))
+                }
             }
-        }
+        }.eraseToAnyPublisher()
     }
     
+}
+
+struct OpenAICompletionsBody: Encodable {
+    let model: String
+    let prompt: String
+    let temperature: Float?
+    let max_tokens: Int?
+}
+
+struct OpenAICompletionsResponse: Decodable {
+    let id: String
+    let choices: [OpenAICompletionChoice]
+}
+
+struct OpenAICompletionChoice: Decodable {
+    let text: String
 }
